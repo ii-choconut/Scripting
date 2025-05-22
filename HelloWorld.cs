025542daa415170ff045af5d49d3d4c9629da48c
ii-choconut <alliyahkatriannadizon@gmail.com>
2025-05-22 14:02:12 +0800

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Output to the console greetings to the world
+        print("Hello, World!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
